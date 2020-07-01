5bc2a29f5dfc314edc9ccc4c26d1219e8b4f0ffc
danielmarques <danielvictorsm@gmail.com>
2020-07-01 11:15:23 -0300

    Vue.js project

---
+++ client/src/components/home/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <p v-show="mensagem" class="centralizado">{{mensagem}}</p>
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="Filtre pelo título"
+    />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva
+            v-meu-transform:scale.animate="1.2"
+            :url="foto.url"
+            :titulo="titulo.url"
+          />
+          <meu-botao
+            tipo="button"
+            rotulo="REMOVER"
+            @botaoAtivado="remove(foto)"
+            :confirmacao="true"
+            estilo="perigo"
+          />
+          <!-- estilo="padrao" -->
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from "../shared/painel/Painel.vue";
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
+
+export default {
+  components: {
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
+  },
+
+  data() {
+    return {
+      titulo: "Photos",
+      fotos: [],
+      filtro: "",
+      mensagem: ""
+    };
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  methods: {
+    remove(foto) {
+      this.resource.delete({ id: foto._id }).then(
+        () => {
+          let indice = this.fotos.indexOf(foto);
+          this.fotos.splice(indice, 1);
+          this.mensagem = "Foto removida com sucesso";
+        },
+        err => {
+          console.log(err);
+          this.mensagem = "Não foi possível remover a foto";
+        }
+      );
+    }
+  },
+
+  created() {
+    this.resource = this.$resource("v1/fotos{/id}");
+    this.resource
+      .query()
+      .then(res => res.json())
+      .then(fotos => ((this.fotos = fotos), err => console.log(err)));
+
+    this.$http.get("http://localhost:3000/v1/fotos");
+  }
+};
+</script>
+
+<style>
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
